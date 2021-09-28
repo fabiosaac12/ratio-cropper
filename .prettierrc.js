69b934f13f6bbea0ec3a30252ac8ffcfff7c2e5c
Fabio Isaac <fabiosaac12@gmail.com>
2021-09-28 15:01:21 -0400

    project initialization

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
