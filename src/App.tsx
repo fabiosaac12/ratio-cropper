7d18b15b86abcb4040f7a6b91cb291f48b595c26
Fabio Isaac <fabiosaac12@gmail.com>
2021-10-14 07:10:56 -0400

    feat: splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { MainStackNavigator } from './navigation/MainStackNavigator';
 import { ImageHandlerProvider } from './providers/ImageHandler';
 import { ModalProvider } from './providers/Modal';
 import { PermissionsProvider } from './providers/Permissions';
 import { ThemeProvider } from './providers/Theme';
+import SplashScreen from 'react-native-splash-screen';
 
 export const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <PermissionsProvider>
